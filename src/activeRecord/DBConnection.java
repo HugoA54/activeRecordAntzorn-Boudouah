572d8c1528ee3135c5e37cff4522b97994957775
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2025-11-18 11:43:49 +0100

    [Feature] Ajout de setNomDB

--- src/activeRecord/DBConnection.java
+++ src/activeRecord/DBConnection.java
@@ -3,17 +3,17 @@ package activeRecord;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBConnection {
 
     private static DBConnection instance;
+
     private Connection connection;
 
     private String userName = "root";
-    private String password = ""; // MAMP : root
+    private String password = "";      // MAMP : root
     private String serverName = "localhost";
-    private String portNumber = ""; // MAMP = 8889
+    private String portNumber = "";    // MAMP : 8889
     private String dbName = "testpersonne";
 
     private DBConnection() {
@@ -23,7 +23,9 @@ public class DBConnection {
                 urlDB += ":" + portNumber;
             }
             urlDB += "/" + dbName + "?useSSL=false&serverTimezone=UTC";
+
             connection = DriverManager.getConnection(urlDB, userName, password);
+
         } catch (SQLException e) {
             throw new RuntimeException("Connexion à la base impossible : " + e.getMessage(), e);
         }
@@ -37,6 +39,26 @@ public class DBConnection {
     }
 
     public Connection getConnection() {
+        if (connection == null) {
+            try {
+
+
+            String urlDB = "jdbc:mysql://" + serverName;
+            if (!portNumber.isEmpty()) {
+                urlDB += ":" + portNumber;
+            }
+            urlDB += "/" + dbName + "?useSSL=false&serverTimezone=UTC";
+
+            connection = DriverManager.getConnection(urlDB, userName, password);
+            } catch (SQLException e) {
+                throw new RuntimeException("Connexion à la base impossible : " + e.getMessage(), e);
+            }
+        }
         return connection;
+    }
+
+    public void setNomDB(String dbName) {
+        this.dbName = dbName;
+        this.connection = null;
     }
 }
